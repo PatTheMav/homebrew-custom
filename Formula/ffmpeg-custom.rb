4fc34dc387e177506de823bc5ccfa0ee775ecafa
PatTheMav <patthemav+github@gmail.com>
2019-02-06 11:02:24 +0100

    Removed bottles from custom formulas for now

--- Formula/ffmpeg-custom.rb
+++ Formula/ffmpeg-custom.rb
@@ -6,12 +6,12 @@ class FfmpegCustom < Formula
   revision 5
   head "https://github.com/FFmpeg/FFmpeg.git"
 
-  bottle do
-    rebuild 1
-    sha256 "2ddcdf6d1a06694eaf262c8a06a2d919eeb3047984e77449761a9c7834b7135b" => :mojave
-    sha256 "6d04eca21b46264566102aaeae2d030776f71bc75a2587c8a5b9600e7b9c711b" => :high_sierra
-    sha256 "bacf852c966847ef0a54e6c4a8e5e78920f898c6e734b68be07f3f6815fa1a3d" => :sierra
-  end
+  # bottle do
+  #   rebuild 1
+  #   sha256 "2ddcdf6d1a06694eaf262c8a06a2d919eeb3047984e77449761a9c7834b7135b" => :mojave
+  #   sha256 "6d04eca21b46264566102aaeae2d030776f71bc75a2587c8a5b9600e7b9c711b" => :high_sierra
+  #   sha256 "bacf852c966847ef0a54e6c4a8e5e78920f898c6e734b68be07f3f6815fa1a3d" => :sierra
+  # end
 
   option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
   option "with-fdk-aac", "Enable the Fraunhofer FDK AAC library"
