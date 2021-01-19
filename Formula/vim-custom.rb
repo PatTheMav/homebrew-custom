2d45b8bccfac9d693cbae5ad4f1652450b557046
PatTheMav <PatTheMav@users.noreply.github.com>
2021-01-19 14:51:09 +0100

    vim 8.2.2350

--- Formula/vim-custom.rb
+++ Formula/vim-custom.rb
@@ -2,8 +2,8 @@ class VimCustom < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v8.2.2200.tar.gz"
-  sha256 "bb2025a2d8e271be0c73483d754272b86a95261090a5e9c2e27c1f6ca8ea3c9c"
+  url "https://github.com/vim/vim/archive/v8.2.2350.tar.gz"
+  sha256 "6af358c1614680591218b5fc2dde29441689d499381438a5d3b9484d461e7b23"
   head "https://github.com/vim/vim.git"
 
   option "with-gettext", "Build vim with National Language Support (translated messages, keymaps)"
@@ -27,9 +27,6 @@ class VimCustom < Formula
     because: "vim-custom and vim both install vi* binaries"
 
   def install
-    # Fix error: '__declspec' attributes are not enabled
-    ENV.append_to_cflags "-fdeclspec" if OS.mac?
-
     # https://github.com/Homebrew/homebrew-core/pull/1046
     ENV.delete("SDKROOT")
 
