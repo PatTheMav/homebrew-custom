c8bfd7f1eba5d198b2d452365f49f9c55a3ff8e9
PatTheMav <PatTheMav@users.noreply.github.com>
2022-02-27 14:44:39 +0100

    vim 8.2.4550

--- Formula/vim-custom.rb
+++ Formula/vim-custom.rb
@@ -2,8 +2,8 @@ class VimCustom < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 25 releases on multiples of 25
-  url "https://github.com/vim/vim/archive/v8.2.3750.tar.gz"
-  sha256 "fbbb7892d9064a52b58a7c5530faee2e3797cf630094e9015ac4e2ae189bd4d3"
+  url "https://github.com/vim/vim/archive/v8.2.4450.tar.gz"
+  sha256 "b0a5acbe83bbdd4b1412abd9dc2ae2e3593c6cff8ff11c551fda3e6e2a87ec81"
   head "https://github.com/vim/vim.git", branch: "master"
 
   option "with-gettext", "Build vim with National Language Support (translated messages, keymaps)"
