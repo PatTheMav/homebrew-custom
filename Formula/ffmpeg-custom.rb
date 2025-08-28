3ce91271e5251f3ff62aa2b71f03cb2a70fa4e6a
PatTheMav <PatTheMav@users.noreply.github.com>
2025-08-28 23:12:25 +0200

    ffmpeg 8.0
    Closes #151.
    
    Signed-off-by: Patrick Heyer <9436503+PatTheMav@users.noreply.github.com>

--- Formula/ffmpeg-custom.rb
+++ Formula/ffmpeg-custom.rb
@@ -1,10 +1,10 @@
 class FfmpegCustom < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-7.1.1.tar.xz"
-  sha256 "733984395e0dbbe5c046abda2dc49a5544e7e0e1e2366bba849222ae9e3a03b1"
+  url "https://ffmpeg.org/releases/ffmpeg-8.0.tar.xz"
+  sha256 "b2751fccb6cc4c77708113cd78b561059b6fa904b24162fa0be2d60273d27b8e"
   license "GPL-2.0-or-later"
-  head "https://github.com/FFmpeg/FFmpeg.git"
+  head "https://github.com/FFmpeg/FFmpeg.git", branch: "master"
 
   livecheck do
     url "https://ffmpeg.org/download.html"
