0cb9b520dda854eb174dd037a3c31559664c1140
Patrick Heyer <9436503+PatTheMav@users.noreply.github.com>
2026-06-08 22:12:57 +0000

    zsh-xcode-completions: add 1.6.0 bottle.

--- Formula/zsh-xcode-completions.rb
+++ Formula/zsh-xcode-completions.rb
@@ -6,6 +6,12 @@ class ZshXcodeCompletions < Formula
 
   head "https://github.com/keith/zsh-xcode-completions.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/PatTheMav/homebrew-custom/releases/download/zsh-xcode-completions-1.6.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2a328a2f68cb85cd6d0a6b7498d37b479683eb899595c2e3f5d3cf4bdc6935b3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a45e2fb1c73b0d478763873160bf5276761b6f8077e1f75f825f1f34867d932b"
+  end
+
   depends_on "jq"
   depends_on :macos
 
